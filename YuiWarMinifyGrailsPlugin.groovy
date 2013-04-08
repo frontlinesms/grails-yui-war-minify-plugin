1b38871cbb8f2e6838c2f3cfb49cd968eb785b59
Alex Anderson <alex@frontlinesms.com>
2013-04-08 17:24:42 +0300

    Removed bad email from YuiWarMinifyGrailsPlugin.

--- YuiWarMinifyGrailsPlugin.groovy
+++ YuiWarMinifyGrailsPlugin.groovy
@@ -3,7 +3,7 @@ class YuiWarMinifyGrailsPlugin {
 	def grailsVersion = "2.0 > *"
 	def title = 'YUI War Minify Plugin'
 	def author = 'Alex Anderson'
-	def authorEmail = 'http://www.github.com/alxndrsn'
+	def authorEmail = ''
 	def description = 'Grails plugin to run yui-compressor on css and javascript resources at WAR creation time.'
 	def documentation = 'http://github.com/frontlinesms/grails-yui-war-minify-plugin'
 	def license = 'APACHE'
