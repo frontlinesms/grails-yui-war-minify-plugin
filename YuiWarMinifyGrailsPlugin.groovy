16c04dbd9e257f417182ccf3ad9477e7020ed8de
Alex Anderson <alex@frontlinesms.com>
2013-07-31 11:26:17 +0300

    [RELEASE: 1.3]

--- YuiWarMinifyGrailsPlugin.groovy
+++ YuiWarMinifyGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class YuiWarMinifyGrailsPlugin {
-	def version = '1.3-SNAPSHOT'
+	def version = '1.3'
 	def grailsVersion = "2.0 > *"
 	def title = 'YUI War Minify Plugin'
 	def author = 'Alex Anderson'
